ca0816e24a842dfeb88be1023a72621d4d6c2863
Jennifer <jennifer@Zipcoders-MacBook-Pro-9.local>
2023-04-05 17:56:28 -0400

    got through part of it, working as a cohort to get these done woohoo

---
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/Node.java
@@ -0,0 +1,19 @@
+package com.zipcodewilmington.singlylinkedlist;
+
+public class Node<T> {
+    T data;
+    Node<T> next;
+
+    public Node(T data){
+        this.data = data;
+        this.next = null;
+    }
+
+    public void setNext(Node<T> next){
+        this.next = next;
+    }
+
+    public T getData(){
+        return data;
+    }
+}
--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -1,7 +1,28 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+import java.util.Comparator;
+
 /**
  * Created by leon on 1/10/18.
  */
-public class SinglyLinkedList {
+public class SinglyLinkedList <T extends Comparable> {
+    Node<T> head;
+
+    public void add(T value) {
+        if (head == null) {
+            head = new Node<T>(value);
+        } else {
+            Node<T> current = head;
+            while (current != null) {
+                current = current.next;
+
+            }
+            current.setNext(new Node<T>(value));
+        }
+    }
+
+    public boolean contains(T expected){
+
+        return false;
+    }
 }
